2972b07aa2291670e25c421d7662ca5dc2f698f9
iara.lima <102303737+iiiiiillllllllllllllllllll@users.noreply.github.com>
2023-04-13 08:29:14 -0400

    Exportação Projeto

---
+++ script/index.js
@@ -0,0 +1,41 @@
+function insereTexto()
+{document.getElementById('sobre-portfolio').innerHTML = 'Projeto desenvolvido por mim em junho de 2022!!';}
+
+function insereTextoV()
+{document.getElementById('conteudo-box1').innerHTML = 'Tocar violão é o hobbie que pratico há mais tempo, comecei com 6 anos e até hoje continuo mesmo que não seja na mesma frequência. Gosto de praticar porque ajuda a minha concentração e me sinto mais conectada comigo mesma. :)';}
+
+function insereTextoVolei()
+{document.getElementById('conteudo-box2').innerHTML ='Outra atividade que gosto de realizar é jogar vôlei, me ajuda a desestressar e a se divertir também, além de fazer bem a saúde.';}
+
+function insereTextoL()
+{document.getElementById('conteudo-box3').innerHTML = 'Passei a gostar de ler e ler em uma frequência maior depois que descobri meus gêneros favoritos. Gosto de livros filosóficos, que ensinam algo, históricos, de ficção científica, e  de alguns romances específicos.';}
+
+function insereTextoM()
+{document.getElementById('conteudo-box4').innerHTML = 'Agora, o que eu mais passo tempo fazendo, que é ouvir música. Apesar de tocar violão desde nova, não escutava muito a músicas, pois os gêneros que eu tocava/toco (música clássica e "modão") não faziam/fazem parte do meu estilo musical. Mas depois encontrei meu estilo passei a ouvir músicas 24/7.';}
+
+
+function mostraEuG(img){
+    img.src="img/eumain.jpg";
+}
+function mostraMiniEu(img){
+    img.src="img/minieu.jpg";
+}
+
+function mostraEu(img){
+    img.src="img/euv.jpg";
+}
+function mostraMarceline(img){
+    img.src="img/marceline.jpg";
+}
+
+
+function mostraHp(img){
+    img.src="img/hp.jpg";
+}
+function mostraLivros(img){
+    img.src="img/livros.jpg";
+}
+
+function infoad(){
+    alert("Segue informações adicionais sobre mim! :)");
+}
